f16192772de7bfbe6bce528e3469648e65d25a34
maxim_stoma <maksim.stoma@mail.ru>
2022-05-14 22:08:07 +0300

    the first task solved

--- task000_theSecondDigit/Program.cs
+++ task000_theSecondDigit/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 999)
+{
+    Console.WriteLine("Я зря сказал трёхзначное?");
+}
+else if (num < 100)
+{
+    Console.WriteLine("Так, тут не хватает цифр");
+}
+else
+{
+    int mult1 = num % 100;
+    int mult2 = mult1 / 10;
+    Console.WriteLine($"Вторая цифра введённого числа {mult2}");
+}
+Console.ReadLine();
