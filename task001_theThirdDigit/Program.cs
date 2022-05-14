c5dae45624b4d6a26570ab8b573fe9bded2b7d10
maxim_stoma <maksim.stoma@mail.ru>
2022-05-14 23:06:06 +0300

    the second task solution paused

--- task001_theThirdDigit/Program.cs
+++ task001_theThirdDigit/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100)
+{
+    Console.WriteLine("Ну...третьей цифры тут нет.");
+}
+else
+{
+    
+}
