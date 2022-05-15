163cd9b2e50af971851c25b5aa860d605a76870e
maxim_stoma <maksim.stoma@mail.ru>
2022-05-15 14:05:53 +0300

    the third task solved

---
+++ task002_weekend/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day > 7)
+{
+    Console.WriteLine($"Ну и где ты видел {day} дней в неделе?");
+}
+else if (day < 1)
+{
+    Console.WriteLine($"Серьёзно? {day} день недели?");
+}
+else
+{
+    if (day < 6)
+    {
+        Console.WriteLine($"Тяжкий будний день...");
+    }
+    else
+    {
+        Console.WriteLine($"Выходной!");
+    }
+}
+Console.ReadLine();
